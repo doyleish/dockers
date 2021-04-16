3a38950edd96942f9fb6ed631ea7208f31159641
Ryan Doyle <ryan@rdoyle.net>
2021-04-15 20:50:39 -0700

    Update nginx w/ php

--- goldeneye/web/start.bash
+++ goldeneye/web/start.bash
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run -p 80 -p 443 -v /websrv:/websrv -v /mnt/store0:/mnt/store0 goldeneye/nginx
+docker run -d -p 80:80 -p 443:443\
+    -v /websrv:/websrv:ro\
+    -v /etc/letsencrypt/live:/etc/letsencrypt/live:ro\
+    -v /etc/letsencrypt/archive:/etc/letsencrypt/archive:ro\
+    -v /mnt/store0:/mnt/store0:ro\
+    goldeneye/nginx
+
