19ccc8bfd45aae72a2c95a6d8f04f7364eb4f6bf
Ryan Doyle <ryan@rdoyle.net>
2024-02-08 23:40:36 +0000

    Clean up unused containers

--- goldeneye/renew-certs.bash
+++ goldeneye/renew-certs.bash
@@ -4,5 +4,5 @@
 #docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot certonly -n --standalone -d minio.rdoyle.net -m ryan@rdoyle.net --agree-tos
 #
 # Renewal
-#docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot renew -v -n --standalone -m ryan@rdoyle.net --agree-tos
+docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot renew -v -n --standalone -m ryan@rdoyle.net --agree-tos
 docker exec nginx nginx -s reload
