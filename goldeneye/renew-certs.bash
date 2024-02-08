3f01a11975567df76f3af314b8885576010bc670
Ryan Doyle <ryan@rdoyle.net>
2024-02-08 01:53:08 +0000

    Refresh

--- goldeneye/renew-certs.bash
+++ goldeneye/renew-certs.bash
@@ -1,9 +1,8 @@
 #!/bin/bash
 #
 # Uncomment to cut new certs
-# docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot certonly -n --standalone -d grafana.rdoyle.net -m ryan@rdoyle.net --agree-tos
-# docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot certonly -n --standalone -d jellyfin.rdoyle.net -m ryan@rdoyle.net --agree-tos
+#docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot certonly -n --standalone -d minio.rdoyle.net -m ryan@rdoyle.net --agree-tos
 #
 # Renewal
-docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot renew -n --standalone -m ryan@rdoyle.net --agree-tos
+#docker run -it --rm --name certbot -v "`pwd`/data/letsencrypt/etc:/etc/letsencrypt" -p 80:80 certbot/certbot renew -v -n --standalone -m ryan@rdoyle.net --agree-tos
 docker exec nginx nginx -s reload
